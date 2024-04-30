875cd575b7990707a520318d7345f7031f049c2e
William Felipe <Williamfelipe3105@gmail.com>
2024-04-30 14:54:30 -0300

    Vídeo em Widgets

--- video.py
+++ video.py
@@ -3,12 +3,7 @@ from kivy.uix.video import Video
 
 class MinhaApp(App):
     def build(self):
-        video = Video(source = 'c:Users/aluno.sesipaulista/Videos/20770858-hd_1080_1920_30fps.mp4')
-        video.state = 'play'
-        video.options = {'eos' : 'loop'}
-        video.allow_stretch = True
-
-        return Video
+        return Video(source = 'C:/Users/aluno.sesipaulista\Downloads/20770858-hd_1080_1920_30fps.mp4')
     
 if __name__ == "__main__":
     MinhaApp().run()
